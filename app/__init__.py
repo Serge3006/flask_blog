2b01de0d164189876f9a68bad973da730346da0e
Serge3006 <chavezferia.justinian@gmail.com>
2021-03-17 22:51:42 +0100

    first commit: login functionality working

---
+++ app/__init__.py
@@ -0,0 +1,36 @@
+import os
+from flask import Flask, jsonify
+from flask import request, render_template, redirect, url_for, session, flash
+from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from datetime import datetime
+from config import config
+from flask_login import LoginManager
+
+bootstrap = Bootstrap()
+moment = Moment()
+db = SQLAlchemy()
+login_manager = LoginManager()
+login_manager.login_view = "auth.login"
+
+def create_app(config_name):
+    app = Flask(__name__)
+    app.config.from_object(config[config_name])
+    config[config_name].init_app(app)
+
+    bootstrap.init_app(app)
+    moment.init_app(app)
+    db.init_app(app)
+    login_manager.init_app(app)
+
+    from .main import main as main_blueprint
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(main_blueprint)
+    app.register_blueprint(auth_blueprint, url_prefix="/auth")
+
+    return app
